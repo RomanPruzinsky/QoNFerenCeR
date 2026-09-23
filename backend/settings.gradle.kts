b73ebcd01a7c229e655c0879eb556564d84e7e5f
Roman Pružinský <xpruzir00@stud.fit.vut.cz>
2026-09-23 15:58:29 +0200

    feat(BE): Renamed root poroject name

--- backend/settings.gradle.kts
+++ backend/settings.gradle.kts
@@ -2,6 +2,6 @@ plugins {
 	id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
-rootProject.name = "backend"
+rootProject.name = "QoNFerenCer-BE"
 
 includeBuild("../shared")
